d2d9a0e92ac8150e2357c7f5700f1c0f98d2f5cb
sghaida <saddam.abughaida@gmail.com>
2023-05-01 18:05:23 +0200

    add slice operations

---
+++ src/collections/slice_ops.go
@@ -0,0 +1,82 @@
+package collections
+
+// List is generic slice
+type List[A any] []A
+
+// Reducer defines the shape of the reduce function
+type Reducer[A, B any] func(acc B, value A) B
+
+// Mapper defines the shape of the Map function
+type Mapper[A, B any] func(elm A) B
+
+// ListOps provide the slice| array operations
+type ListOps[A any] interface {
+	Size() int
+	Foreach(fn func(elm A))
+	Append(elm A) List[A]
+	Prepend(elm A) List[A]
+	Take(n int) List[A]
+}
+
+// Size return size
+func (a List[A]) Size() int {
+	return len(a)
+}
+
+// Foreach apply fn for each element in A
+func (a List[A]) Foreach(fn func(elm A)) {
+	for _, value := range a {
+		fn(value)
+	}
+}
+
+// Append appends element to the List
+func (a List[A]) Append(elm A) List[A] {
+	arr := make(List[A], len(a), len(a)+1)
+	copy(arr, a)
+	arr = append(arr, elm)
+	return arr
+}
+
+// Prepend prepends element to the List
+func (a List[A]) Prepend(elm A) List[A] {
+	arr := make(List[A], 0, len(a)+1)
+	arr = append(arr, elm)
+	arr = append(arr, a...)
+	return arr
+}
+
+// Take takes n element from the slice
+func (a List[A]) Take(n int) List[A] {
+	if n < 0 {
+		return make(List[A], 0)
+	}
+	arr := make(List[A], 0, n)
+	for i := 0; i < n && i < a.Size(); i++ {
+		arr = append(arr, a[i])
+	}
+	return arr
+}
+
+// Map apply F: A => B: List[B]
+func Map[A, B any](lst List[A], mapper Mapper[A, B]) List[B] {
+	acc := make([]B, 0, len(lst))
+	for _, elm := range lst {
+		acc = append(acc, mapper(elm))
+	}
+	return acc
+}
+
+// Reduce Apply F: A => B: B
+func Reduce[A, B any](lst List[A], reducer Reducer[A, B]) B {
+	var acc B
+	return FoldLeft(lst, acc, reducer)
+}
+
+// FoldLeft applies F: (B, A) => B: B
+func FoldLeft[A, B any](lst List[A], acc B, fn Reducer[A, B]) B {
+	for _, value := range lst {
+		acc = fn(acc, value)
+	}
+	return acc
+}
---
+++ src/collections/slice_ops_test.go
@@ -0,0 +1,69 @@
+package collections_test
+
+import (
+	"fp/src/collections"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestList_Size(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	assert.Equal(t, lst.Size(), 5)
+}
+
+func TestList_Foreach(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	ch := make(chan int, len(lst))
+	lst.Foreach(func(elm int) {
+		ch <- elm
+	})
+	close(ch)
+	var acc int
+	for elm := range ch {
+		acc += elm
+	}
+	assert.Equal(t, acc, 15)
+}
+
+func TestList_Map(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	res := collections.Map(lst, func(elm int) string {
+		return strconv.Itoa(elm * 2)
+	})
+	assert.Equal(t, res[0], "2")
+}
+
+func TestReduce(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	res := collections.Reduce(lst, func(acc int, elm int) int {
+		return acc + elm
+	})
+	assert.Equal(t, res, 15)
+}
+
+func TestList_Append(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	res := lst.Append(6)
+	assert.Equal(t, res.Size(), 6)
+	assert.Equal(t, res[5], 6)
+}
+
+func TestList_Prepend(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	res := lst.Prepend(0)
+	assert.Equal(t, res.Size(), 6)
+	assert.Equal(t, res[0], 0)
+}
+
+func TestList_Take(t *testing.T) {
+	lst := collections.List[int]{1, 2, 3, 4, 5}
+	res := lst.Take(3)
+	assert.Equal(t, res.Size(), 3)
+
+	res = lst.Take(-1)
+	assert.Equal(t, res.Size(), 0)
+
+	res = lst.Take(10)
+	assert.Equal(t, res.Size(), 5)
+}
